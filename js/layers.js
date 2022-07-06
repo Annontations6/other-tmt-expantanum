a2bdf331eea922cd5c6ec448dccef6f5bed4eaf8
Annontations6 <106895624+Annontations6@users.noreply.github.com>
2022-07-05 22:38:46 -0300

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -21,8 +21,5 @@ addLayer("G", {
         return new ExpantaNum(1)
     },
     row: 0, // Row the layer is in on the tree (0 is the first row)
-    hotkeys: [
-        {key: "p", description: "P: Reset for prestige points", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
-    ],
     layerShown(){return true}
 })
